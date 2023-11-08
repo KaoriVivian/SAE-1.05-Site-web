0b9719f61ec834dd74fe42b72fcc01046daf4aea
Viviane Laoue <102098957+KaoriVivian@users.noreply.github.com>
2023-11-08 09:09:54 +0100

    files

---
+++ passion-marathons-etud/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Passion Marathons</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="./assets/css/style.css" type="text/css" media="all">
+	
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Lato&family=Oswald&display=swap" rel="stylesheet"> 
+</head>
+<body>
+	<div id="bloc_principal">
+		<header>
+			<img src="./assets/images/banniere.png" alt="Passion Marathons - Agence spécialisées dans les marathons">
+			<h1>Aller au bout de soi-même</h1>
+		</header>
+		<nav>
+			<a href="./pages/marathons.html">
+				<img src="./assets/images/menu/dest.svg" alt="Les marathons dans le monde">
+				Marathons
+			</a>
+			<a href="./pages/alimentation.html">
+				<img src="./assets/images/menu/conseils.svg" alt="Conseils pour la préparation d'un marathon">
+				conseils
+			</a>
+			<a href="#">
+				<img src="./assets/images/menu/tems.svg" alt="Témoignages de marathoniens">
+				Témoignages
+			</a>
+			<a href="#">
+				<img src="./assets/images/menu/nous.svg" alt="Equipe de Passion Marathons">
+				L'équipe
+			</a>
+		</nav>
+		<section id="alaune">
+			<img src="./assets/images/home/alaune/new-york.jpg" alt="Ligne d'arrivée du marathon de New York">
+			<div>
+				<p>By Greg / Juillet 2023</p>
+				<p>L'aventure d'une vie</p>
+				<a href="#">Lire plus</a>
+			</div>
+		</section>
+		<section id="articles">
+			<div class="titre_section">
+				<h2>Nos derniers articles</h2>
+			</div>
+			<div>
+				<a href="#">
+					<figure>
+						<div>
+							<img src="./assets/images/home/articles/medoc.jpg" alt="Marathon du Médoc">
+						</div>
+						<figcaption>
+							<h2>Le marathon du médoc</h2>
+							<p>Course - France - Nouvelle aquitaine</p>
+						</figcaption>
+					</figure>
+				</a>
+				<a href="#">
+					<figure>
+						<div>
+							<img src="./assets/images/home/articles/cognac.jpg" alt="Marathon de Cognac">
+						</div>
+						<figcaption>
+							<h2>le marathon de cognac</h2>
+							<p>Course - France - Nouvelle aquitaine</p>
+						</figcaption>
+					</figure>
+				</a>
+				<a href="#">
+					<figure>
+						<div>
+							<img src="./assets/images/home/articles/dietetique.jpg" alt="Préparation marathon">
+						</div>
+						<figcaption>
+							<h2>Alimentation avant la course</h2>
+							<p>Conseils</p>
+						</figcaption>
+					</figure>
+				</a>
+			</div>
+		</section>
+		<section id="temoignage" >
+			<div class="titre_section">
+				<h2>Sur la ligne d'arrivée</h2>
+			</div>
+			<div>
+				<blockquote>
+					<em>Au départ : une adrénaline de ouf, au 32ème : un coup de barre XXL</em>
+				</blockquote>
+				<p class="texte_paragraphe"> L’hymne américain retenti, le coup de canon part…la course est lancée.
+				A moi New York mais avant il va falloir passer le Verazano Brigde.
+				ça grimpe dur mais qu’est ce que c’est beau. Ensuite c’est 42,190 
+				dans les 5 quartiers de New York avec du monde (beaucoup de monde) sur le bord de route. 
+				L’ambiance est dingue. Les new yorkais sont fous surtout à Brooklyn. 
+				Le parcours est compliqué car beaucoup de ligne droite en faut plat montant et les ponts sont de vrais col ! L’arrivée à central Park est grandiose avec du monde mêlés au couleur de l’automne. A noté qu’il y a très peu de nourriture au ravitaillement si ce n’est deux points où ils distribuent des gels. 
+				Il faut mieux prévoir ! Sinon il y a à boire tous les miles (1,6km) sauf sur les ponts.</p>
+				<img src="./assets/images/home/temoignage/new-york-ligne.jpg">
+			</div>
+		</section>
+		<section id="instagram">
+			<div class="titre_section">
+				<h2>Passion Marathons sur Instagram</h2>
+			</div>
+			<div>
+			<?php
+        $numbers = array();
+        $arraysWidth = sizeof($numbers);
+        $STR = "";
+
+        while ($arraysWidth != 10) {
+            $n = random_int(1, 20);
+            if (!(in_array($n,$numbers))) {
+                array_push($numbers, $n);
+                $arraysWidth++;
+            }
+        } 
+
+        foreach($numbers as $num)
+        {
+            $STR .= $num."; ";
+        }
+        echo sizeof($numbers);
+        echo ("<p>".$STR."</p>");
+        ?>
+			</div>
+			<div>
+				<a href="#">
+	               	Rejoignez-nous sur Insta
+				</a>
+			</div>
+		</section>
+	</div>
+	<footer>
+		<p>Copyright : Passion Marathons 2015/2023 - contac@passionmarathons.com</p>
+		<a href="#">Les auteurs du site </a>
+		<a href="#bloc_principal">Haut de la page</a>
+	</footer>
+</body>
+</html>
+			
+			
+				
