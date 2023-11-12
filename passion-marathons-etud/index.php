6b557ad13c74969cb3b4c09309c12d1b03d3071d
Viviane Laoue <102098957+KaoriVivian@users.noreply.github.com>
2023-11-12 13:27:31 +0100

    Redirections vers auteurs.html + header sur auteurs.html

--- passion-marathons-etud/index.php
+++ passion-marathons-etud/index.php
@@ -33,7 +33,7 @@
 				<img src="./assets/images/menu/tems.svg" alt="Témoignages de marathoniens">
 				Témoignages
 			</a>
-			<a href="#">
+			<a href="./pages/auteurs.html">
 				<img src="./assets/images/menu/nous.svg" alt="Equipe de Passion Marathons">
 				L'équipe
 			</a>
