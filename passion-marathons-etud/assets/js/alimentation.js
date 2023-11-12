3446bfef479248009f78a314a23a4955af94857c
Mr-Shadow-Mc <gameurminecraft22@gmail.com>
2023-11-12 17:14:44 +0100

    Update Visual

--- passion-marathons-etud/assets/js/alimentation.js
+++ passion-marathons-etud/assets/js/alimentation.js
@@ -10,14 +10,24 @@ window.addEventListener("DOMContentLoaded",function(){
     const articleApres = document.getElementById("apresArticle");
 
     btnAvant.addEventListener('click',function(){
+        btnAvant.classList.add("btnSelected");
+        btnPendant.classList.remove("btnSelected");
+        btnApres.classList.remove("btnSelected");
+
         console.log("avant");
         ShowArticle('block', 'none', 'none');
     })
     btnPendant.addEventListener('click',function(){
+        btnAvant.classList.remove("btnSelected");
+        btnPendant.classList.add("btnSelected");
+        btnApres.classList.remove("btnSelected");
         console.log("pendant");
         ShowArticle('none', 'block', 'none');
     })
     btnApres.addEventListener('click',function(){
+        btnAvant.classList.remove("btnSelected");
+        btnPendant.classList.remove("btnSelected");
+        btnApres.classList.add("btnSelected");
         console.log("apres");
         ShowArticle('none', 'none', 'block');
     })
