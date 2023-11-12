15d30aad630a14c9fbc5c46ade6815b8f18541b2
Mr-Shadow-Mc <gameurminecraft22@gmail.com>
2023-11-12 01:30:59 +0100

    Update calculatrice

--- passion-marathons-etud/assets/js/calculatrice.js
+++ passion-marathons-etud/assets/js/calculatrice.js
@@ -1,15 +1,23 @@
-window.addEventListener("DOMContentLoaded", function()
-{
+window.addEventListener("DOMContentLoaded", function () {
+    /// API
     const vitesse = document.getElementById('vitesse'); // Inputs
     const temps = document.getElementById('temps'); // Inputs
     const poids = document.getElementById('poids'); // Inputs
-    const calculer = document.getElementById('calculer'); // Button
+    const btnCalculer = document.getElementById('btnCalculer'); // Button
     const nbKcal = document.getElementById('nbKcal'); // Div
     const nbMacarons = document.getElementById('nbMacarons'); // Div
 
+    /// HELPFUL
+    const error_indicator = document.getElementById('error_indicator');
+    let bool_error;
+
     //Calculer lorsque l'utilisateur appuis sur le button...
-    calculer.addEventListener('click', function()
-    {
+    btnCalculer.addEventListener('click', function () {
+        CheckInputs();
+        Calculer();
+    });
+
+    function Calculer() {
         let vitesseVal = Number(vitesse.value);
         let tempsVal = Number(temps.value);
         let poidsVal = Number(poids.value);
@@ -19,7 +27,36 @@ window.addEventListener("DOMContentLoaded", function()
 
         let macarons = (kcal / 100);
 
-        nbKcal.innerHTML = kcal + " Kcal consommées.";
-        nbMacarons.innerHTML = macarons + " macarons / calories consommées.";
-    })
+        if (bool_error !== true) {
+            nbKcal.innerHTML = kcal + " Kcal consommées.";
+            nbMacarons.innerHTML = macarons + " macarons / calories consommées.";
+        }
+        else
+        {
+            nbKcal.innerHTML = null;
+            nbMacarons.innerHTML = null;
+        }
+
+    }
+
+    function CheckInputs() {
+        if (vitesse.value === '' || temps.value === '' || poids.value === '') {
+            error("Veuillez remplir les informations manquants.");
+            return;
+        } else {
+            hideError();
+        }
+    };
+
+
+    function error(Message) {
+        bool_error = true;
+        error_indicator.style.display = 'contents';
+        error_indicator.innerHTML = Message;
+    }
+
+    function hideError() {
+        bool_error = false;
+        error_indicator.style.display = 'none';
+    }
 })
